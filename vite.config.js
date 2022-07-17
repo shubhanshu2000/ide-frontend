b465b39dbf08253bec0e715b10a953da672888d5
Shubhanshu Singh <shubhanshusingh1796@gmail.com>
2022-07-17 09:52:23 +0530

    added engines to package.jsonand changed vite congig to original

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    "@": resolve(__dirname, "src"),
-  },
   plugins: [react()],
 });
