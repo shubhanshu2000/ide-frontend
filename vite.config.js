0b3baffa97a5d11073005a420ee59190f779a400
Shubhanshu Singh <shubhanshusingh1796@gmail.com>
2022-07-17 09:30:04 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [/^node:.*/],
-    },
+  alias: {
+    "@": resolve(__dirname, "src"),
   },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
 });
